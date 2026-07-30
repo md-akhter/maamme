36c018b914c96eafcdbd7711addb35b8bfc7f355
md-akhter <md.akhterhossain5051@gmail.com>
2026-07-30 14:01:10 +0600

    Add files via upload

--- admin.js
+++ admin.js
@@ -1,40 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  signOut
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+function login() {
+  let email = document.getElementById("email").value;
+  let pass = document.getElementById("password").value;
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAB9n2iK6wNdvugRNynR9S7yUP_E1Kl_Xg",
-  authDomain: "maamme.firebaseapp.com",
-  projectId: "maamme",
-  storageBucket: "maamme.firebasestorage.app",
-  messagingSenderId: "962319743416",
-  appId: "1:962319743416:web:83ce97db81775d06c7b74d",
-  measurementId: "G-2PWZ2WBMB2"
-};
+  if (email == "md.akhterhossain5051@gmail.com" && pass == "mdakhter50@") {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("dashboard").style.display = "block";
+  } else {
+    alert("Wrong Email or Password");
+  }
+}
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-window.login = function () {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  signInWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      document.getElementById("login").style.display = "none";
-      document.getElementById("dashboard").style.display = "block";
-    })
-    .catch(() => {
-      alert("Wrong Email or Password");
-    });
-};
-
-window.logout = function () {
-  signOut(auth).then(() => {
-    document.getElementById("dashboard").style.display = "none";
-    document.getElementById("login").style.display = "block";
-  });
-};
+function logout() {
+  document.getElementById("dashboard").style.display = "none";
+  document.getElementById("login").style.display = "block";
+}
